21bddb5f665f00e80c9b737a40c712b9fdf3d1c1
Farzad Afshari <far.afshari@gmail.com>
2012-04-03 22:41:29 -0400

    404 day

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Hungryfun</title>
 	<link href='http://fonts.googleapis.com/css?family=News+Cycle' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="style.css" type="text/css"/>
 <script src="http://code.jquery.com/jquery-latest.js"></script>
@@ -15,18 +15,8 @@
 <body onResize="ContentManager.fixSelectedScreen();">
 	<?php require('header.php');?>
 	<div id="content">
-			<div id="content-portal-c">
-				<a id="content-portal" class="index-content-button" onClick="ContentManager.expandPortal();">
-				STUDENT<BR>PORTAL
-				</a>
-			</div>	
-			<div id="content-learn-c" onClick="ContentManager.expandLearn();">
-				<a id="content-learn" class="index-content-button">
-				LEARNING<BR>CENTER
-				</a>
-			</div>
+			<span style="font-size: 160px;">404</span>
 	</div>
-
 	<?php require('footer.php');?>
 </body>
 </html>
